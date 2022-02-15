7617998266098d364abef47a706dc5133b1d9581
Jeff <zhsu@amwares.com>
2022-02-15 15:59:56 +0800

    开始搞了，很多不知道的东西，原来大写就是export的。

--- src/main/kotlin/cn/jeff/tools/gpc/ConverterInGoLang.go
+++ src/main/kotlin/cn/jeff/tools/gpc/ConverterInGoLang.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"./misc"
+	"fmt"
+	"strings"
+	"time"
+)
+
+const filePath = "TTN2/YOHGAS/GPC/"
+
 func main() {
-	println("这样也可以？")
+	println("开始转换……")
+	t1 := time.Now().UnixMilli()
+
+	for _, gpcFile := range misc.WalkDir(filePath, ".GPC") {
+		pngFile := strings.Replace(gpcFile, ".GPC", ".png", 1)
+		convertGpcToPng(gpcFile, pngFile)
+	}
+
+	t2 := time.Now().UnixMilli()
+	println("转换完成。")
+	fmt.Printf("用时：%d毫秒。", t2-t1)
+}
+
+func convertGpcToPng(gpcFile string, pngFile string) {
+	fmt.Printf("%s ==> %s\n", gpcFile, pngFile)
 }
