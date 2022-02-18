1c975202706e2e6496f29d5b1409355419ca4a2d
Jeff <zhsu@amwares.com>
2022-02-18 15:23:56 +0800

    还原。

--- src/main/kotlin/cn/jeff/tools/gpc/ConverterInGoLang.go
+++ src/main/kotlin/cn/jeff/tools/gpc/ConverterInGoLang.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/png"
 	"os"
 	"path/filepath"
 	"strings"
@@ -41,8 +42,8 @@ func convertGpcToPng(gpcFile string, pngFile string) {
 		}
 	}
 	fmt.Printf("width=%d, height=%d\n", width, height)
-	//out, _ := os.Create(pngFile)
-	//_ = png.Encode(out, img)
+	out, _ := os.Create(pngFile)
+	_ = png.Encode(out, img)
 }
 
 func decodeGpc(data []byte) (width, height int, pixels [][]color.RGBA) {
